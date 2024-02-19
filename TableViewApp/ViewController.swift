af2833be028af0f1c8b13af4752b5df4183bf792
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-19 12:32:17 +0530

    Table view Code

--- TableViewApp/ViewController.swift
+++ TableViewApp/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    var tableData:[String] = ["a","b","c","d","e"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+//        
+//        cell.textLabel?.text = tableData[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tableData[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    var rowindex:Int?
+    @IBOutlet weak var inputfield: UITextField!
+    @IBOutlet weak var tbl: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tbl.dataSource = self
+        tbl.delegate = self
+                
+                // Register cell class if necessary
+        tbl.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    @IBAction func addclick(_ sender: Any){
+        tableData.append(inputfield.text!)
+        tbl.beginUpdates()
+        tbl.insertRows(at: [IndexPath(row: tableData.count - 1, section: 0)], with: .automatic)
+        tbl.endUpdates()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        inputfield.text = tableData[indexPath.row]
+        rowindex = indexPath.row
+    }
+    
+    @IBAction func deleteBtnclick(_ sender: Any) {
+        tableData.remove(at: rowindex!)
+        tbl.beginUpdates()
+        tbl.deleteRows(at: [IndexPath(row: rowindex!, section: 0)], with: .automatic)
+        tbl.endUpdates()
+    }
 }
 
