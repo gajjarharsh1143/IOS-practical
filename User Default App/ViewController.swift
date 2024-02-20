adfd7f14693e96b62fa1e8c1872852d60cc03a8d
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-02-20 12:46:13 +0530

    User Defaults  like cookie

--- User Default App/ViewController.swift
+++ User Default App/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inp1: UITextField!
+    @IBOutlet weak var inp2: UITextField!
+    
+    let user = UserDefaults.standard;
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let name = user.object(forKey: "name");
+        let surname = user.object(forKey: "surname");
+        
+        if(name != nil || surname != nil)
+        {
+            inp1.text =  name as? String;
+            inp2.text = surname as? String;
+        }
+        
     }
-
-
+    
+    @IBAction func saveClick(_ sender: Any) {
+        user.set("\(inp1.text!)", forKey: "name")
+        user.set("\(inp2.text!)", forKey: "surname")
+    }
+    
+    @IBAction func deleteBtn(_ sender: Any) {
+        user.removeObject(forKey: "name")
+        user.removeObject(forKey: "surname")
+        
+        inp1.text = ""
+        inp2.text = ""
+    }
+    
 }
 
