befca73461c1a0698ae515faeb6e32d93656698c
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-14 14:51:52 +0530

    Map view

--- MapView App/ViewController.swift
+++ MapView App/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MAPVIEWOBJ: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // statue of unity
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 21.8380, longitude: 73.7191);
+        
+        // home
+//        let location:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 21.150650, longitude: 72.757611);
+        
+//        let location:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0);
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001);
+        
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: location, span: span);
+        
+        self.MAPVIEWOBJ.setRegion(region, animated: true);
+        
+        // Add anotatioan
+        
+        let annotation:MKPointAnnotation = MKPointAnnotation();
+        annotation.coordinate = location
+        
+        annotation.title = "Gajjar Harsh";
+        annotation.subtitle = "ABC"
+        
+        MAPVIEWOBJ.addAnnotation(annotation)
     }
-
-
 }
 
