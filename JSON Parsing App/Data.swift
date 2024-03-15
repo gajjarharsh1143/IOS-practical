e6f6c985e0158b480253d8397767493cb6450e3f
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-15 13:14:38 +0530

    JSON

---
+++ JSON Parsing App/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  JSON Parsing App
+//
+//  Created by MSCIT on 15/03/24.
+//
+
+import Foundation
--- JSON Parsing App/ViewController.swift
+++ JSON Parsing App/ViewController.swift
@@ -5,13 +5,69 @@
 //  Created by MSCIT on 15/03/24.
 //
 
+//[
+//    {
+//        "id": 101,
+//        "name": "Gajjar Harsh",
+//        "Age": 21
+//    },
+//    {
+//        "id": 102,
+//        "name": "Ravi Medhat",
+//        "Age": 21
+//    }
+//]
+
+
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let url = Bundle.main.url(forResource: "data", withExtension: "json");
+//        print("File Path : \(url!)");
+//        
+////       let JSONData = url!;
+//        
+//        let JSONData = try! Data(contentsOf: url!)
+//        print("File Size : \(JSONData)");
+//        
+//        let json = try? JSONSerialization.jsonObject(with: JSONData);
+////        print(json as Any);
+//        
+//        if let dictionary = json as? [String: Any]{
+//            let id = dictionary["id"] as? Int{
+//                print("Id : \(id)")
+//            }
+//        }
+        let url = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        let jsonData = url
+        
+        let data = try? Data(contentsOf: jsonData!)
+        let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+        
+        if let dictionary = json as? [String: Any] {
+            
+            if let id = dictionary["id"] as? Int {
+                print("id is \(id)")
+            }
+            
+            if let name = dictionary["name"] as? String {
+                print("Name is \(name)")
+            }
+            
+            if let age = dictionary["marks"] as? Int {
+                print("marks is \(age)")
+            }
+        }
+        
     }
 
 
