c37b698fbd7008e4d28a30dc1476a52ba6e58e51
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-15 12:41:01 +0530

    Gesture APP

--- Gestures App/ViewController.swift
+++ Gestures App/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        S1.backgroundColor = UIColor.orange;
+    }
+    
+    @IBOutlet var S1: UIView!
+    @IBAction func onSwipeRight(sender: Any){
+        print("Gesture done of swipe right");
+        if S1.backgroundColor == UIColor.orange {
+            S1.backgroundColor = UIColor.blue;
+        }
+        else if S1.backgroundColor == UIColor.blue{
+            S1.backgroundColor = UIColor.green;
+        }
+        else{
+            S1.backgroundColor = UIColor.orange;
+        }
     }
 
+    @IBAction func onLongPress(sender: Any){
+        print("Gesture Long Press");
+        if S1.backgroundColor == UIColor.black {
+            S1.backgroundColor = UIColor.white;
+        }
+        else{
+            S1.backgroundColor = UIColor.black;
+        }
+    }
 
 }
 
