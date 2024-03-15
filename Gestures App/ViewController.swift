34ee4ff00167f50b737ceca94d157bdc3f2d485c
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-15 12:19:02 +0530

    Initial Commit

---
+++ Gestures App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gestures App
+//
+//  Created by MSCIT on 15/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
