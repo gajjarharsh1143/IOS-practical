d261bfdf4cb3d280cab69a476cbf3f2702414174
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-18 12:07:09 +0530

    PLIST App

--- PLIST App/ViewController.swift
+++ PLIST App/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var datavalues = [String]();
+        
+        let path = Bundle.main.url(forResource: "fruits", withExtension: "plist");
+        
+        let dict = NSDictionary(contentsOf: path!);
+        
+        datavalues = dict?.allValues as! [String];
+        
+        print(datavalues);
+        
     }
 
 
