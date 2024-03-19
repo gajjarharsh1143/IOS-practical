e97cdda2b62eca0a1bb37c15f0e90f80b241266e
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-19 12:08:17 +0530

    Scroll View app

--- ScrollVieew App/ViewController.swift
+++ ScrollVieew App/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollviewObj: UIScrollView!
+    var img : UIImageView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollviewObj.minimumZoomScale = 0.25;
+        scrollviewObj.maximumZoomScale = 2;
+        
+        scrollviewObj.delegate = self;
+        
+        let image = UIImage(named: "img1.jpeg");
+        img = UIImageView(image: image);
+        
+        scrollviewObj.contentSize = image!.size
+        
+        scrollviewObj.addSubview(img!);
+        
+        scrollviewObj.zoomScale = 2;
     }
-
-
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return img;
+    }
+    
 }
 
