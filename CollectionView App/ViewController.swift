37edcc64c9b450bf916f8a8d5a2a1421672708dc
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-19 12:48:04 +0530

    Collection View App

--- CollectionView App/ViewController.swift
+++ CollectionView App/ViewController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout  {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2000;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for:    indexPath)
 
+                cell.backgroundColor  = self.customeColor();
+
+                return cell
+    }
+    func customeColor()-> UIColor{
+
+            let red = CGFloat(drand48())
+
+            let green = CGFloat(drand48())
+
+            let blue = CGFloat(drand48())
+        
+        let alpha = CGFloat(drand48());
+
+            
+
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+
+        }
+    
+    @IBOutlet weak var cv: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
         // Do any additional setup after loading the view.
     }
 
